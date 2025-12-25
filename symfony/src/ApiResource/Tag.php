a6632e83ee94f9590a0388c0b7b563760ccceccc
Takashi Kanemoto <ttskch@gmail.com>
2025-12-25 14:13:19 +0900

    feat: 🎸 第3章: Articleエンティティにタグを付けられるように

---
+++ symfony/src/ApiResource/Tag.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\ApiResource;
+
+use ApiPlatform\Metadata\ApiResource;
+
+#[ApiResource]
+class Tag
+{
+    public const array ALLOWED_TAGS = [
+        'tag1',
+        'tag2',
+        'tag3',
+        'tag4',
+        'tag5',
+        'tag6',
+        'tag7',
+        'tag8',
+        'tag9',
+        'tag10',
+    ];
+
+    public function __construct(
+        public int $id,
+        public string $label,
+    ) {
+    }
+}
