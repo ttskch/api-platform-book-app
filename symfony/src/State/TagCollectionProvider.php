5a7dd254616e5227b8457e35b1fb32f1b246491a
Takashi Kanemoto <ttskch@gmail.com>
2026-01-23 12:50:41 +0900

    feat: 🎸 第7章: TagCollectionProviderをページネーションに対応

--- symfony/src/State/TagCollectionProvider.php
+++ symfony/src/State/TagCollectionProvider.php
@@ -3,17 +3,28 @@
 namespace App\State;
 
 use ApiPlatform\Metadata\Operation;
+use ApiPlatform\State\Pagination\ArrayPaginator;
+use ApiPlatform\State\Pagination\Pagination;
 use ApiPlatform\State\ProviderInterface;
 use App\ApiResource\Tag;
 
 class TagCollectionProvider implements ProviderInterface
 {
+    public function __construct(private Pagination $pagination)
+    {
+    }
+
     public function provide(Operation $operation, array $uriVariables = [], array $context = []): object|array|null
     {
-        return array_map(
+        $tags = array_map(
             fn (string $tag, int $i) => new Tag($i + 1, $tag),
             Tag::ALLOWED_TAGS,
             array_keys(Tag::ALLOWED_TAGS),
         );
+
+        $offset = $this->pagination->getOffset($operation, $context);
+        $limit = $this->pagination->getLimit($operation, $context);
+
+        return new ArrayPaginator($tags, $offset, $limit);
     }
 }
